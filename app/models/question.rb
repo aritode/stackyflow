b5a0fae2b9b7cf07a2513454faece33ddbb3184e
aritode <arian2k@gmail.com>
2016-08-16 01:53:45 +0300

    Added factory :answer association :question and max-length Question :title

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ActiveRecord::Base
   has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
+  validates :title, length: { maximum: 255 }
   validates :title, :body, length: { minimum: 10 }
 end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :answer do
+    # association :question
+    question
     body "MyAnswerText"
   end
 end
